d5ff3df3d3a5b41114e40bf7fd8b8c141914d45c
CARLOS EMILIO SARMIENTO ALMANZA <22030535@itcelaya.edu.mx>
2026-04-29 11:42:07 -0600

    Remove raw JSON data display from UI

--- src/components/PageWithRawData.tsx
+++ src/components/PageWithRawData.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import { DataRenderer } from './DataRenderer';
 import styles from './PageWithRawData.module.css';
 
@@ -9,32 +9,10 @@ interface PageWithRawDataProps {
 }
 
 export function PageWithRawData({ data }: PageWithRawDataProps) {
-  const [open, setOpen] = useState(false);
-
   return (
     <div className={styles.wrapper}>
-      {/* Pretty view */}
+      {/* Pretty view only */}
       <DataRenderer data={data} />
-
-      {/* Raw JSON section */}
-      <div className={styles.rawSection}>
-        <button
-          className={`${styles.rawToggle} ${open ? styles.rawToggleOpen : ''}`}
-          onClick={() => setOpen(prev => !prev)}
-        >
-          <span className={styles.rawIcon}>{open ? '▼' : '▶'}</span>
-          <span>Datos crudos de la API</span>
-          <span className={styles.badge}>JSON</span>
-        </button>
-
-        {open && (
-          <div className={styles.rawContent}>
-            <pre className={styles.pre}>
-              {JSON.stringify(data, null, 2)}
-            </pre>
-          </div>
-        )}
-      </div>
     </div>
   );
 }
